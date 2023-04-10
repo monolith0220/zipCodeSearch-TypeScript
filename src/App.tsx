077d648995e79b8e7dbadfdcf9b1f2478c844867
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-04-10 10:56:39 +0900

    郵便番号7桁以上はエラー表示

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,18 @@ type ZipcodeResult = {
 	status: number;
 };
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
 	const [postalCode, setPostalCode] = useState<string>("");
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string>("");
 	const [address, setAddress] = useState<string>("");
 
 	const handlePostalCodeChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		setPostalCode(event.target.value);
+		if (event.target.value.length <= 7) {
+			setPostalCode(event.target.value);
+		} else {
+			setError("郵便番号が7桁を超えています");
+		}
 	};
 
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
@@ -28,7 +32,9 @@ function App(): JSX.Element {
 		setError("");
 		try {
 			const response = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${postalCode}`);
+			console.log(response);
 			const data: ZipcodeResult = await response.json();
+			console.log(data);
 			if (data.results && data.results.length > 0) {
 				setAddress(data.results[0].address1 + data.results[0].address2 + data.results[0].address3);
 			} else {
@@ -43,7 +49,8 @@ function App(): JSX.Element {
 	return (
 		<div>
 			<header>
-				<img src="logo.png" alt="Logo" />
+				<h1>郵便番号検索</h1>
+				<p>※ハイフンなしで入力してください</p>
 				<form onSubmit={handleSubmit}>
 					<input type="text" value={postalCode} onChange={handlePostalCodeChange} />
 					<button type="submit">検索</button>
@@ -51,11 +58,11 @@ function App(): JSX.Element {
 			</header>
 			<main>
 				{isLoading && <div>検索中...</div>}
-				{error && <div>{error}</div>}
+				{error && <div style={{ color: "red" }}>{error}</div>}
 				{address && <div>{address}</div>}
 			</main>
 		</div>
 	);
-}
+};
 
 export default App;
